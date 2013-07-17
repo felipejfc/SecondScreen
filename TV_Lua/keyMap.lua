3c596b8797357cac4935642962a44fd125fbb7bd
Fagner Gomes <fagner.gomes@ccc.ufcg.edu.br>
2013-07-17 13:28:09 -0300

    refatorado o codigo em lua
    criado uma funçao que verifica se o teclado ta habilitado ou nao, basta
    enviar "KEYRUNNING" ainda nao é final, tenho que mapear de forma menos
    obivia as teclas

--- TV_Lua/keyMap.lua
+++ TV_Lua/keyMap.lua
@@ -1,6 +1,48 @@
 keyMap ={}
+local volume = 0
+---KEYBOARD VERYFY
+keyMap['KEYRUNNING'] = function(copas, skt, keyRunning)
+		copas.send(skt, tostring(keyRunning))
+end
+---CURSOR
+keyMap['UPP'] = 'CURSOR_UP'
+keyMap['DOW'] = 'CURSOR_DOWN'
+keyMap['LEF'] = 'CURSOR_LEFT'
+keyMap['RIG'] = 'CURSOR_RIGHT'
+keyMap['ENT'] = 'ENTER'
+---OPTION
+keyMap['BACK'] = 'BACK'
+keyMap['MENU'] = 'MENU'
+keyMap['EXIT'] = 'EXIT'
+keyMap['INFO'] = 'INFO'
+keyMap['EPG'] = 'EPG'
+keyMap['CHLIST'] = 'CH_LIST'
+---COlOR
+keyMap['RED'] = 'RED'
+keyMap['GRE'] = 'GREEN'
+keyMap['BLU'] = 'BLUE'
+keyMap['YEL'] = 'YELLOW'
+---Basic Zapper
+keyMap['CUP'] = 'CHANNEL_UP'
+keyMap['CDW'] = 'CHANNEL_DOWN'
+keyMap['MUT'] =  function ()
+	if control.getVolume () == 0 then 
+		control.setVolume(volume)
+	else
+		control.setVolume(0)
+	end
 
-keyMap['01'] = {class = 'key', key = 'CURSOR_UP', type = ''}
-keyMap['02'] = {class = 'key', key = 'CURSOR_DOWN', type = ''}
+end
+keyMap['VUP'] = function ()
+control.volumeUp()
+volume = control.getVolume()
+end
+keyMap['VDW'] = function() 
+control.volumeDown()
+volume = control.getVolume()
+end
+
+--~ keyMap['CUP'] = function () print('Channel UP') end
+--~ keyMap['CDW'] = function () print('Channel Down') end
 
 return keyMap
--- TV_Lua/remoteControl.lua
+++ TV_Lua/remoteControl.lua
@@ -2,44 +2,96 @@ local register = event.register
 local unregister = event.unregister
 local socket = require("socket")
 local copas = require('copas')
+require("basicZapper")
+
+
 local server = assert(socket.bind("*", 1212))
 local typeKey = {'press','release'}
 local keyMap = require('keyMap')
+local rprint = function (...)
+local rkeyRunning = nil
+local rEdit = nil
+print('=====SecondScreen=====', ...)
+end
 
-handlers = function() return end
+local handlers = function() return end
+
 event.register = function(...)
+rprint('registrei')
 handlers = ...
 register(...)
 end
 event.unregister = function (...)
-handlers =  function() return end
+rprint('disregistrei')
+--~ handlers =  function() return end
 unregister(...)
 end
 
+local function sendKey(key)
+	handlers ({class = 'key', key = keyMap[key], type = typeKey[1]})
+	handlers ({class = 'key', key = keyMap[key], type = typeKey[2]})
+end
 
-local function secondScreen(key)
-print(key)
-	if keyMap[key] then 
-		keyMap[key].class = typeKey[1] 
-		handlers (keyMap[key])
-		keyMap[key].class = typeKey[2] 
-		handlers (keyMap[key])
+local function selectAction(key,skt)
+    if keyMap[key] then 
+        if type(keyMap[key]) == "string" then
+           sendKey(key)
+        else 
+            keyMap[key](copas,skt,rkeyRunning )
+        end
+        return true
+    end
+    return false
+end
+
+local function keyboardHandler(skt, data)
+	if rEdit and rkeyRunning then
+		if(rEdit._password == nil)then
+			rEdit:text(rEdit:text().. data)
+			rEdit._cursor = #rEdit:text() + #data
+		else
+			rEdit._password = rEdit._password .. data
+			rEdit._cursor = #rEdit._password + #data
+		end
+		
+		rEdit:draw()
+		canvas:flush()
 	end
 end
 
 function echoHandler(skt)
   while true do
     local data = copas.receive(skt)
-    if data == "quit" then
-      break
-    end
-    copas.send(skt, 'recebi '..data)
-    secondScreen(data)
+    if data then 
+		copas.send(skt, 'recebi '..data..'\n')
+			
+		if data == "quit" then
+		  break
+		else
+			if not (selectAction(data, skt)) then
+					keyboardHandler(skt, data)
+			end
+				
+		end
+	end
   end
 end
 
 copas.addserver(server, echoHandler, 0.1)
+function setKeyboard (keyboarda)
+rprint('atribui um teclado = ', type(keyboarda))
+rEdit = keyboarda._textEdit
+end
 
+function openKeyboardOnDivice(bollRunning)
+rprint('habilitei o teclado = ', bollRunning)
+rkeyRunning= bollRunning
+
+end
+function setEdit(textEdit)
+rEdit = textEdit
+
+end
 function refreshControl()
 event.timer(200,function()
 copas.step(0.1)
